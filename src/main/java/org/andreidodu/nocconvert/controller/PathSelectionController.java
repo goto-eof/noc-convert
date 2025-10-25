1068d395d8a2d102795823f5dab4fc7e0211ef00
Andrei Dodu <andrei.dodu@hotmail.it>
2025-10-25 18:49:41 +0200

    refactor: wip

--- src/main/java/org/andreidodu/nocconvert/controller/PathSelectionController.java
+++ src/main/java/org/andreidodu/nocconvert/controller/PathSelectionController.java
@@ -1,13 +1,20 @@
 package org.andreidodu.nocconvert.controller;
 
+import lombok.Getter;
 import org.andreidodu.nocconvert.dto.PathSelectionDTO;
+import org.andreidodu.nocconvert.dto.PathSelectionRawDTO;
 import org.andreidodu.nocconvert.gui.components.TextfieldButtonComponent;
 import org.andreidodu.nocconvert.util.FileSystemSupportGuiUtil;
+
+import java.nio.file.Path;
+import java.util.function.Consumer;
 
 public class PathSelectionController {
     private final PathSelectionDTO pathSelectionDTO;
     private final FileSystemSupportGuiUtil fileSystemSupportGuiUtil;
 
+    @Getter
+    private final PathSelectionRawDTO pathSelectionRawDTO = PathSelectionRawDTO.builder().build();
 
     public PathSelectionController(PathSelectionDTO pathSelectionDTO) {
         this.pathSelectionDTO = pathSelectionDTO;
@@ -23,19 +30,24 @@ public class PathSelectionController {
 
     private void addEventListenersForSourceComponent() {
         TextfieldButtonComponent sourceComponent = pathSelectionDTO.sourceComponent();
-        addBrowseDirectoryEventListener(sourceComponent);
+        addBrowseDirectoryEventListener(sourceComponent, pathSelectionRawDTO::setSourceDirectory);
     }
 
     private void addEventListenersForDestinationComponent() {
         TextfieldButtonComponent destinationComponent = pathSelectionDTO.destinationComponent();
-        addBrowseDirectoryEventListener(destinationComponent);
+        addBrowseDirectoryEventListener(destinationComponent, , pathSelectionRawDTO::setDestinationDirectory);
     }
 
-    private void addBrowseDirectoryEventListener(TextfieldButtonComponent component) {
+    private void addBrowseDirectoryEventListener(TextfieldButtonComponent component, Consumer<Path> pathConsumer) {
         component.getButton()
                 .addActionListener(e -> fileSystemSupportGuiUtil.selectDirectory()
-                        .ifPresent(text -> component.getTextField().setText(text))
+                        .ifPresent(text -> {
+                            Path path = Path.of(text);
+                            pathConsumer.accept(path);
+                            component.getTextField().setText(path.getFileName().toString());
+                        })
                 );
     }
+
 
 }
---
+++ src/main/java/org/andreidodu/nocconvert/dto/PathSelectionRawDTO.java
@@ -0,0 +1,16 @@
+package org.andreidodu.nocconvert.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.nio.file.Path;
+
+@Builder
+@Getter
+@Setter
+public class PathSelectionRawDTO {
+    private Path sourceDirectory;
+    private Path destinationDirectory;
+
+}
