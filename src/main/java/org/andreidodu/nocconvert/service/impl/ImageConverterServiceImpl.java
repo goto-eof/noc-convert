ac56e4e75e14f84765ec60b41385870835b5e414
Andrei Dodu <andrei.dodu@hotmail.it>
2025-10-28 00:27:11 +0100

    fix: to jpg conversion

--- src/main/java/org/andreidodu/nocconvert/service/impl/ImageConverterServiceImpl.java
+++ src/main/java/org/andreidodu/nocconvert/service/impl/ImageConverterServiceImpl.java
@@ -33,7 +33,7 @@ import java.util.function.Consumer;
 public class ImageConverterServiceImpl implements ImageConverterService {
     private static final Logger log = LogManager.getLogger(ImageConverterServiceImpl.class);
 
-    private static final List<String> NO_ALPHA_FORMAT = List.of("bmp", "wbmp");
+    private static final List<String> NO_ALPHA_FORMAT = List.of("bmp", "wbmp", "jpg", "jpeg");
     private ImageWriter writer;
     private ImageOutputStream currentOutputStream;
     private ImageInputStream imageInputStream;
