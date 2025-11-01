4ab17f05744e474a325096fa991acc02592cef08
Andrei Dodu <andrei.dodu@hotmail.it>
2025-11-01 17:47:29 +0100

    fix: wip

--- src/main/java/org/andreidodu/nocconvert/gui/controller/ConversionController.java
+++ src/main/java/org/andreidodu/nocconvert/gui/controller/ConversionController.java
@@ -272,7 +272,7 @@ public class ConversionController {
         }
 
         Optional.ofNullable(paths).ifPresent(paths -> {
-            applicationStatusLabel.setText("Converted " + processedItems + " of " + paths.size() + " Images (" + passes.get() + " passed/" + failures.get() + " failed). Please wait.");
+            applicationStatusLabel.setText("Converted " + processedItems + " of " + paths.size() + " Images (" + passes.get() + " passed | " + failures.get() + " failed). Please wait.");
             long now = System.currentTimeMillis();
             if (now - lastTime >= 1000) {
                 lastTime = now;
